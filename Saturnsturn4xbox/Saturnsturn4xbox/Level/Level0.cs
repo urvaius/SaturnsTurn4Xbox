e0d7dac969cd519610eaaa2525abbd82afc44a05
Dave Arneson <urvaius@gmail.com>
2016-07-11 20:18:02 -0700

    started base not working yet

---
+++ Saturnsturn4xbox/Saturnsturn4xbox/Level/Level0.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SaturnsTurn3.Level
+{
+    class Level0
+    {
+
+    }
+}
---
+++ Saturnsturn4xbox/Saturnsturn4xbox/Utility/InitializeEnemies.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SaturnsTurn3.Utility
+{
+	public class InitializeEnemies
+	{
+		public void Initialize(string enemyName,string listName,string spawnTimeName,string prevSpawnTimeName,Timespan prevSpawnTime,TimeSpan spawnTime)
+
+		{
+
+		}
+
+
+	}
+}
